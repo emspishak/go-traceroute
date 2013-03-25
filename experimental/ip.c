3775105eb5fb082fc65420aa3ffe2c258922e58d
Eric Spishak <emspishak@gmail.com>
2013-03-24 23:32:37 -0700

    Read the destination IP address from an environment variable.

--- experimental/ip.c
+++ experimental/ip.c
@@ -2,11 +2,12 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/socket.h>
 
-#define DST_IP "192.168.1.12"
+#define DEFAULT_DST_IP "192.168.1.12"
 #define PORT 35984
 #define UDP_HDR_SIZE 8
 
@@ -19,11 +20,19 @@ struct udp_header {
 
 // A simple test of raw sockets. Creates an IP payload of a UDP packet and
 // message and sends it.
+//
+// Set the DST_IP environment variable to indicate to which IP address the
+// packet should be sent.
 
 int main(int argc, char *argv[]) {
     // Make sure everything is the correct size.
     assert(sizeof(short) == 2);
     assert(sizeof(struct udp_header) == UDP_HDR_SIZE);
+
+    char* dstIP = getenv("DST_IP");
+    if (dstIP == NULL) {
+        dstIP = DEFAULT_DST_IP;
+    }
 
     int sd = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
 
@@ -46,7 +55,7 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in dstAddr;
     dstAddr.sin_family = AF_INET;
     dstAddr.sin_port = htons(PORT);
-    inet_pton(AF_INET, DST_IP, &dstAddr.sin_addr);
+    inet_pton(AF_INET, dstIP, &dstAddr.sin_addr);
 
     if (sendto(sd, buf, totalLength, 0, (struct sockaddr*) &dstAddr, sizeof(dstAddr)) == -1) {
         perror("error sending");
